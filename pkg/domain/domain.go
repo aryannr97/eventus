d954d0cff1a4c3b4b17f060f24d896648821c600
Rishabh Nrupnarayan <aryannr97@gmail.com>
2020-05-03 14:10:45 +0530

    Added missing domain pkg

---
+++ pkg/domain/domain.go
@@ -0,0 +1,12 @@
+package domain
+
+type SQSMessageBody struct {
+	Type             string `json:"Type"`
+	MessageId        string `json:"TopicArn`
+	Message          string `json:"Message"`
+	Timestamp        string `json:"Timestamp"`
+	SignatureVersion string `json:"SignatureVersion"`
+	Signature        string `json:"Signature"`
+	SigningCertURL   string `json:"SigningCertURL"`
+	UnsubscribeURL   string `json:"UnsubscribeURL"`
+}
